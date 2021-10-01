8cc3f8286ef40791688d6b4753d513718d93d697
= <=>
2021-10-01 20:22:30 -0300

    initial commit

---
+++ src/http.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer);
+
+module.exports = { httpServer, io };
---
+++ src/server.js
@@ -0,0 +1,3 @@
+const { httpServer } = require("./http");
+
+httpServer.listen(3000, () => console.log("running..."));
