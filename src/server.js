cc74b97939cf1859bb5c7cae7b857f6079e432f5
= <=>
2021-10-02 10:14:20 -0300

    config port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const { httpServer } = require("./http");
 require("./websocket");
 
-// httpServer.listen(process.env.PORT, () => console.log("running..."));
-httpServer.listen(3000, () => console.log("running..."));
+httpServer.listen(process.env.PORT || 3000, () => console.log("running..."));
